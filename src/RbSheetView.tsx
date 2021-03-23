d157292c04ba7d3065de08ea66277702dab7f4e5
SergeyMild <sergeymild@yandex.ru>
2021-03-23 07:54:41 +0200

    Create RbSheetView.tsx

---
+++ src/RbSheetView.tsx
@@ -0,0 +1,24 @@
+import React, {useContext, useRef} from 'react'
+import {View, ViewProps} from 'react-native'
+import {RBContext} from 'react-native-raw-bottom-sheet/src'
+
+interface Props extends ViewProps {}
+
+const RbSheetView: React.FC<Props> = (props) => {
+  const wasLayout = useRef<boolean>(false)
+  const context = useContext(RBContext)
+
+  return (
+    <View
+      {...props}
+      onLayout={(e) => {
+        if (wasLayout.current) return
+        wasLayout.current = true
+        context.setHeight(e.nativeEvent.layout.height)
+      }}>
+      {props.children}
+    </View>
+  )
+}
+
+export default RbSheetView
